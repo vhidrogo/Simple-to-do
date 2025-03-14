4288a82bcb040c82026eb2ce24dcd298a78b0cbf
Victor Hidrogo <victor@nav.it>
2025-03-14 11:52:30 -0700

    Initial Commit

---
+++ SimpleToDoTests/SimpleToDoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleToDoTests.swift
+//  SimpleToDoTests
+//
+//  Created by Victor Hidrogo on 3/14/25.
+//
+
+import Testing
+@testable import SimpleToDo
+
+struct SimpleToDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
